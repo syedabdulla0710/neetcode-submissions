ab38840dcdefc28723916cd4e98495794c0ca254
Syed Abdulla <syedabdulla0710@gmail.com>
2026-08-20 23:25:23 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n= nums.length;
+        for(int i=0;i<n;i++){
+            if (nums[i]<=0){
+                nums[i] = n+1;
+            }
+        }
+        for (int i = 0;i<nums.length;i++){
+            
+            int val = Math.abs(nums[i]);
+            if (val<=nums.length){
+                nums[val-1] = -Math.abs(nums[val-1]);
+            }
+            
+        }
+        for (int i =0;i<nums.length;i++){
+           
+            if (nums[i]>0){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+}
